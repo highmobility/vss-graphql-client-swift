de9f027e9bc117a79b82c82af93baaa3dd5d7f8c
doofyus <mikk.ratsep@gmail.com>
2020-10-14 15:12:21 +0300

    change 'cabin_door_rows'

---
+++ Sources/VSSGraphQLClient/VSSGraphQLOperation.swift
@@ -0,0 +1,21 @@
+//
+//  VSSGraphQLOperation.swift
+//  VSSGraphQLClient
+//
+//  Created by Mikk Rätsep on 14.10.20.
+//
+
+import Foundation
+
+
+public typealias VSSGraphQLOperation = AutoGraphQL.Operation
+
+
+public extension VSSGraphQLOperation {
+
+    static func vehicleQuery(selectionSet: SelectionSet, name: String = "VehicleQuery") -> VSSGraphQLOperation {
+        VSSGraphQLOperation(type: .query, name: name, selectionSet: [
+            Object(name: "vehicle", selectionSet: selectionSet)
+        ])
+    }
+}
